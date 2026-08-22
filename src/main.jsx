4b9f331307c520c26924081717e1ff06b720ebae
gamed24h-lab <gamed24h@gmail.com>
2026-08-22 12:20:14 +0300

    enable central system branding provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import {ThemeProvider} from './core/ThemeContext.jsx';
+import {SystemBrandProvider} from './core/SystemBrandContext.jsx';
 import './styles.css';
 import './themes.css';
 import './branch-branding.css';
 import './developer-footer.css';
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><ThemeProvider><App /></ThemeProvider></React.StrictMode>);
+createRoot(document.getElementById('root')).render(<React.StrictMode><ThemeProvider><SystemBrandProvider><App /></SystemBrandProvider></ThemeProvider></React.StrictMode>);
