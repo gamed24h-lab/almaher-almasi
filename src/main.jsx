9368e3d632665d9afe6f65b91f529aebf311840b
gamed24h-lab <gamed24h@gmail.com>
2026-08-22 12:16:32 +0300

    enable theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import {ThemeProvider} from './core/ThemeContext.jsx';
 import './styles.css';
+import './themes.css';
 import './branch-branding.css';
 import './developer-footer.css';
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
+createRoot(document.getElementById('root')).render(<React.StrictMode><ThemeProvider><App /></ThemeProvider></React.StrictMode>);
