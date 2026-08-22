fb1f544885e4bb3436b562e12a72810d9cb5e92c
gamed24h-lab <gamed24h@gmail.com>
2026-08-22 08:46:41 +0300

    load developer footer styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './branch-branding.css';
+import './developer-footer.css';
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
