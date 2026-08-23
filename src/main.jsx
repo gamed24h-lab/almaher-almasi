f90bf878c228a3abef522ff0eadf08b6bd81841f
gamed24h-lab <gamed24h@gmail.com>
2026-08-23 10:11:10 +0300

    mount global feedback host

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import GlobalFeedback from './components/GlobalFeedback.jsx';
 import {ThemeProvider} from './core/ThemeContext.jsx';
 import {SystemBrandProvider} from './core/SystemBrandContext.jsx';
 import './styles.css';
@@ -8,5 +9,6 @@ import './themes.css';
 import './branch-branding.css';
 import './developer-footer.css';
 import './modal-feedback.css';
+import './feedback.css';
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><ThemeProvider><SystemBrandProvider><App /></SystemBrandProvider></ThemeProvider></React.StrictMode>);
+createRoot(document.getElementById('root')).render(<React.StrictMode><ThemeProvider><SystemBrandProvider><App/><GlobalFeedback/></SystemBrandProvider></ThemeProvider></React.StrictMode>);
