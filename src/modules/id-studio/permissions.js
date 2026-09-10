c66c946a61e05ad8d4f91b4c614b4169762c7401
gamed24h-lab <gamed24h@gmail.com>
2026-09-10 08:07:17 +0300

    feat(id-studio): centralize module permission catalog

---
+++ src/modules/id-studio/permissions.js
@@ -0,0 +1,19 @@
+export const ID_STUDIO_PERMISSIONS=[
+  ['id_studio_access','الدخول إلى ID Studio'],
+  ['id_card_view','عرض البطاقات'],
+  ['id_card_create','إنشاء بطاقة'],
+  ['id_card_edit','تعديل بطاقة'],
+  ['id_card_approve','اعتماد بطاقة'],
+  ['id_card_print','طباعة بطاقة'],
+  ['id_card_export','تصدير PNG / PDF'],
+  ['id_card_reissue','إعادة إصدار بطاقة'],
+  ['id_card_revoke','إلغاء / إيقاف بطاقة'],
+  ['id_card_link_employee','ربط بطاقة بموظف موجود'],
+  ['id_card_view_all_branches','عرض بطاقات كل الفروع'],
+  ['id_card_manage_templates','إدارة قوالب البطاقات'],
+  ['id_card_manage_settings','إدارة إعدادات ID Studio'],
+  ['id_card_import','استيراد بطاقات Excel / CSV'],
+  ['id_card_audit_view','عرض سجل التدقيق والطباعة'],
+];
+
+export const ID_STUDIO_PERMISSION_KEYS=ID_STUDIO_PERMISSIONS.map(([key])=>key);
